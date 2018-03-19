443f6c3730f3b5216fd3d1d19dd3389bc6a1a12c
usagitoneko <hogouxian@gmail.com>
2018-03-19 22:58:32 +0800

    completed fibonacci with exec in A3

--- A3.reversingAst/examply.py
+++ A3.reversingAst/examply.py
@@ -1,17 +1,11 @@
-def fib_recursive(startNumber, endNumber):
-    n = 0
-    cur = f(n)
-    while cur <= endNumber:
-        if startNumber <= cur:
-            print(cur)
-        n += 1
-        cur = f(n)
+def fib():
+    a = 0
+    b = 1
+    while True:            # First iteration:
+        yield a            # yield 0 to start with and then
+        a, b = b, a + b    # a will now be 1, and b will also be 1, (0 + 1)
 
-def fib_iterative(startNumber, endNumber):
-    for cur in F():
-        if cur > endNumber: return
-        if cur >= startNumber:
-            yield cur
-
-for i in fib_iterative(10, 200):
+for i in fib():
     print(i)
+    if i > 100:
+        break
--- A3.reversingAst/reverseAst.py
+++ A3.reversingAst/reverseAst.py
@@ -24,69 +24,95 @@ if __name__ == '__main__':
     print(astor.to_source(asTree))
 
     '''
-def fib_recursive(startNumber, endNumber):
-    n = 0
-    cur = f(n)
-    while cur <= endNumber:
-        if startNumber <= cur:
-            print cur
-        n += 1
-        cur = f(n)
+    def fib():
+        a = 0
+        b = 1
+        while True:            # First iteration:
+            yield a            # yield 0 to start with and then
+            a, b = b, a + b    # a will now be 1, and b will also be 1, (0 + 1)
     '''
-    # n = 0
-    
-    assign1 = ast.Assign(targets=ast.Name(id="x", ctx=ast.Store()),
-                         value=ast.Num(0))
-    
-    assign1 = ast.Assign(targets=ast.Name(id="cur", ctx=ast.Store()),
-                         value=ast.Call(func=ast.Name(id='f', ctx=ast.Store()),
-                                        args=[ast.Name(id='n', ctx=ast.Store())],
-                                        keywords=[]))
-    '''
-    while1 = ast.While(test=ast.Compare(left=ast.Name(id='cur', ctx=ast.Load()),
-                                        opst=[ast.LtE()],
-                                        comparators=[id="endNumber", ctx=ast.Load()],),
-                       body=[ast.If(test=ast.Compare(left=ast.name("startNumber"), ctx=ast.Load(),
-                                                     opst=[ast.LtE()],
-                                                     comparators=[ast.Name("cur"), ctx=ast.Load()],),
-                                    body=ast.Expr(ast.Call(func=ast.Name(id="print", ctx=ast.Load()),
-                                                           args=[ast.Name(id="cur", ctx=ast.Load())],
-                                                           keywords=[])),
-                                    orelse=[]),
-                             ast.AugAssign(target=ast.Name(id="n", ctx=ast.Store()),
-                                           op=ast.Add(),
-                                           value=ast.Num(1)),
-                             ast.Assign(targets=[ast.Name(id="cur", ctx=ast.Store())],
-                                        value=ast.Call(func=ast.Name(id="f", ctx=ast.Load()),
-                                                       args=[ast.Name(id="n", ctx=ast.Load())],
-                                                       keywords=[]))],
-                       orelse=[])
-    funcDef = ast.FuncDef(name="fib_recursive",
-                          args=ast.Args(args=[ast.arg(arg="startNumber",
-                                                      annotation=None),
-                                              asr.arg(arg="endNumber",
-                                                      annotation=None)],
-                                        vararg=None)
+    # a = 0
+    a_eq_0 = ast.Assign(targets=[ast.Name(id="a", ctx=ast.Store())],
+                       value=ast.Num(0))
+    b_eq_1 = ast.Assign(targets=[ast.Name(id="b", ctx=ast.Store())],
+                       value=ast.Num(1))
 
-                          body=[])
+    # yield a
+    yield_a = ast.Expr(value=ast.Yield(ast.Name(id="a", ctx=ast.Load())))
+
+    # a, b
+    left_target = [ast.Tuple(elts=[ast.Name(id="a", ctx=ast.Store()),
+                                   ast.Name(id="b", ctx=ast.Store())],
+                             ctx=ast.Load())]
+
+    # b, a + b
+    right_value = ast.Tuple(elts=[ast.Name(id="b", ctx=ast.Store()),
+                                   ast.BinOp(left=ast.Name(id="a", ctx=ast.Load()),
+                                             op=ast.Add(),
+                                             right=ast.Name(id="b", ctx=ast.Load()))],
+                            ctx=ast.Load())
+
+    # a, b = b, a + b
+    assign1 = ast.Assign(targets=left_target, value=right_value)
 
     '''
-    # cur = f(n)
-    cur=[ast.Name(id="cur", ctx=ast.Store())]
-    fib=ast.Call(func=ast.Name(id="fib", ctx=ast.Load())
-    n=[ast.Name(id="n", ctx=ast.Load())]               
-    curAssign = ast.Assign(cur, fib, n)
-    
-'''
-def fib_iterative(startNumber, endNumber):
-    for cur in F():
-        if cur > endNumber: return
-        if cur >= startNumber:
-            yield cur
-
-for i in fib_iterative(10, 200):
-    print i
+    while True:            # First iteration:
+        yield a            # yield 0 to start with and then
+        a, b = b, a + b    # a will now be 1, and b will also be 1, (0 + 1)
     '''
-    
-    
 
+    while_body = [yield_a, assign1]
+    whileLoop = ast.While(test=ast.NameConstant(value=True),
+                          body=while_body,
+                          orelse=[])
+
+
+
+    fibDef = ast.FunctionDef(name="fib",
+                             args=ast.arguments(args=[],
+                                                vararg=None,
+                                                kwonlyargs=[],
+                                                kw_defaults=[],
+                                                kwarg=None,
+                                                defaults=[]),
+                             body=[a_eq_0,
+                                   b_eq_1,
+                                   whileLoop],
+                             decorator_list=[],
+                             returns=None)
+
+    # print(i)
+    print_i = ast.Expr(value=ast.Call(func=ast.Name(id="print", ctx=ast.Load()),
+                                      args=[ast.Name(id="i", ctx=ast.Load())],
+                                      keywords=[]))
+
+    # if i > 100:
+    #    break
+    if_i = ast.If(test=ast.Compare(left=ast.Name(id="i", ctx=ast.Load()),
+                                   ops=[ast.Gt()],
+                                   comparators=[ast.Num(100)]),
+                  body=[ast.Break()],
+                  orelse=[])
+    '''
+    for i in fib():
+        print(i)
+        if i > 100:
+            break
+    '''
+
+    # fib()
+    fibCall = ast.Call(func=ast.Name(id="fib", ctx=ast.Load()),
+                       args=[],
+                       keywords=[])
+    for_i = ast.For(target=ast.Name(id="i", ctx=ast.Store()),
+                    iter=fibCall,
+                    body=[print_i,
+                          if_i],
+                    orelse=[])
+
+    asTree = ast.Module(body=[fibDef, for_i])
+    ast.fix_missing_locations(asTree)
+    print(astor.to_source(asTree))
+
+    programString = astor.to_source(asTree)
+    exec(programString)
